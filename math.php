fe92d9fcbf7f5f54f3791f60729ec82b1922cd9f
Hannah <hannahshea.consulting@gmail.com>
2020-05-18 11:33:17 -0400

    adding more to math

---
+++ math.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php  
+
+echo 56 + 50;
+echo "<br>";
+echo 56 - 45; 
+echo "<br>";
+echo 56 * 45; 
+echo "<br>";
+echo 56 / 45;
+echo "<br>";
+
+
+echo 45 + 34 * 45 /421 - 45;
+echo "<br>";
+
+//PEMDAS is still a thing... //
+
+echo (5 + 5) * 10;
+
+
+?>
+
+</body>
+</html>
