00e1ca5fc1ae66f468fb41325eb3e55a24062277
Hannah <hannahshea.consulting@gmail.com>
2020-05-19 10:17:59 -0400

    starting arrays

---
+++ arrays.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+ 
+<?php 
+
+//two ways to write arrays, the first is "older" but the second is used more//
+
+// $numberList = array(); //
+// $numberList = []; //
+
+$numberList = array(267,8765,345,'5345',345,'<h1>hello</h1>');
+
+echo $numberList [0];
+
+
+
+?>
+
+    
+</body>
+</html>
--- math.php
+++ math.php
@@ -26,6 +26,14 @@ echo "<br>";
 
 echo (5 + 5) * 10;
 
+echo "<br>";
+
+//Math with variables//
+
+$num1 = 12;
+$num2 = 24; 
+
+echo $num1 * $num2;
 
 ?>
 
